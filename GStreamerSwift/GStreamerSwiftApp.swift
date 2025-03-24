b1362c0ec30a7066c018e83d392d16b41685773b
sujin01 <r5870@naver.com>
2025-03-25 04:48:15 +0900

    Initial Commit

---
+++ GStreamerSwift/GStreamerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GStreamerSwiftApp.swift
+//  GStreamerSwift
+//
+//  Created by 김수진 on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GStreamerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
